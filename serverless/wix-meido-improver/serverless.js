0d27db9bb4e241c14ca7f0ff519ff56bf56ed7d2
Roman Shevchuk <romansh@wix.com>
2019-06-21 11:41:24 +0300

    try to fix build

--- serverless/wix-meido-improver/serverless.js
+++ serverless/wix-meido-improver/serverless.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const favorites_1 = require("./src/favorites");
 const auth_1 = require("./src/auth");
 exports.default = (functionsBuilder) => functionsBuilder
