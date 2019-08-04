41dfa2621bac6b99d4a0d94ca0f1135ff09d461d
Roman Shevchuk <romansh@wix.com>
2019-08-04 21:55:58 +0300

    feature(extension): change popup orders week

--- extension/popup/script.js
+++ extension/popup/script.js
@@ -5,21 +5,45 @@ button.addEventListener('click', event => {
 });
 
 
+let weekIndexToShow = 0;
+const ordersElem = document.getElementById('orders');
+const thisWeekElem = document.getElementsByClassName('this-week')[0];
+const updateDateElem = document.getElementsByClassName('updated-date')[0];
+const notLoadedElem = document.getElementsByClassName('not-loaded')[0];
+
 (async () => {
+  document.getElementsByClassName('prev-week')[0]
+    .addEventListener('click', async () => {
+      weekIndexToShow--;
+      await render();
+    });
+
+  document.getElementsByClassName('next-week')[0]
+    .addEventListener('click', async () => {
+      weekIndexToShow++;
+      await render();
+    });
+
+  thisWeekElem
+    .addEventListener('click', async () => {
+      weekIndexToShow = 0;
+      await render();
+    });
+
   subscribeForStorageChanges(() => render());
   await render();
 })();
 
 
 async function render() {
-  const {ordersPerDay, updatedDate} = await getWorkingWeekOrders(new Date());
-  const ordersElem = document.getElementById('orders');
-  const updateDateElem = document.getElementsByClassName('updated-date')[0];
-  const notLoadedElem = document.getElementsByClassName('not-loaded')[0];
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() + (weekIndexToShow * 7));
+  const {ordersPerDay, updatedDate} = await getWorkingWeekOrders(startDate);
 
   ordersElem.innerText = '';
   updateDateElem.innerText = '';
   notLoadedElem.style.display = updatedDate ? 'none' : 'block';
+  thisWeekElem.classList.toggle('yes', weekIndexToShow === 0);
 
   if (!updatedDate) {
     return;
@@ -27,14 +51,21 @@ async function render() {
 
   updateDateElem.innerText = 'Updated: ' + new Date(updatedDate).toLocaleString();
 
-  ordersPerDay.forEach(order => {
-    const date = new Date(order.date);
+  ordersPerDay.forEach((order, weekDayIndex) => {
+    let date;
+    if (order && order.date) {
+      date = new Date(order.date);
+    } else {
+      date = startDate;
+      date.setDate(date.getDate() + weekDayIndex);
+    }
+
     const weekElem = createWeekElem(
       date,
       order && order.dishName,
       order && order.contractorName
     );
-    weekElem.classList.toggle('today', isSameDay(new Date(order.date), new Date()));
+    weekElem.classList.toggle('today', isSameDay(new Date(date), new Date()));
     ordersElem.appendChild(weekElem);
   })
 }
@@ -50,7 +81,7 @@ function createWeekElem(date, dishName, contractorName) {
     <div class="week__order">
       <div>
         <div class="order">${dishName ? escapeHtml(dishName) : '<i>No order</i>'}</div>
-        <div class="contractor">${escapeHtml(contractorName)}</div>
+        <div class="contractor">${escapeHtml(contractorName || '')}</div>
       </div>
     </div>
   `;
